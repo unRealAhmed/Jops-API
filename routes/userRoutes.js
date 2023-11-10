589c17a02319fdb8cf8d9682046126733e54f842
Ahmed Sayed <therealahme@gmail.com>
2023-11-10 20:55:40 +0200

    refactor some code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,36 @@
 const express = require('express');
 
 // Import authentication controller methods
-const { signUp, login, forgetPassword, resetPassword, updatePassword, protect, restrictTo } = require('../controllers/authController');
+const {
+  signUp,
+  login,
+  forgetPassword,
+  resetPassword,
+  updatePassword,
+  protect,
+  restrictTo,
+} = require('../controllers/authController');
 
-const { getAllUsers, deleteUser } = require('../controllers/userController')
+const { getAllUsers, deleteUser } = require('../controllers/userController');
 
 // Create a router instance
 const router = express.Router();
 
+// Public routes
 router.post('/signup', signUp);
 router.post('/login', login);
 router.post('/forgotPassword', forgetPassword);
 router.patch('/resetPassword/:token', resetPassword);
 router.patch('/updateMyPassword', protect, updatePassword);
 
-router.use(protect, restrictTo('admin'));
+// Protected routes (requires authentication)
+router.use(protect);
 
-router.get('/', getAllUsers)
-router.delete('/:id', deleteUser)
+// Routes restricted to admin only
+router.use(restrictTo('admin'));
 
+router.route('/').get(getAllUsers);
+router.route('/:id').delete(deleteUser);
 
 // Export the router
 module.exports = router;
